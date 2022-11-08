dd287112a146b7a99750ef191b7cd4bf35caabce
Dorani <seif.dorani@gmail.com>
2022-11-08 05:25:39 -0500

    better solution

--- Big_JS_Algos/JS_Algos_Course/3.Sliding_Window/Activities/problem2.js
+++ Big_JS_Algos/JS_Algos_Course/3.Sliding_Window/Activities/problem2.js
@@ -16,3 +16,27 @@
 
 // Space Complexity - O(1)
 
+function minSubArrayLen(nums, sum) {
+    let total = 0;
+    let start = 0;
+    let end = 0;
+    let minLen = Infinity;
+
+    while(start < nums.length) {
+        //if current window doesn't add up to the given sum 
+        if(total < sum && end < nums.length) {
+            //move the window to the right
+            total += nums[end];
+            end++
+        }
+        else if(total >= sum) {
+             minLen = Math.min(minLen, end-start);
+             total -= nums[start];
+             start++;
+        } else {
+            break
+        }
+    }
+
+    return minLen === Infinity ? 0: minLen;
+}
