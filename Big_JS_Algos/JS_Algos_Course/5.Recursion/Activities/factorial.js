b3db8bccee650da7da7d2c4e1303500bde87aabc
Dorani <seif.dorani@gmail.com>
2023-03-09 07:26:52 -0500

    edits

---
+++ Big_JS_Algos/JS_Algos_Course/5.Recursion/Activities/factorial.js
@@ -0,0 +1,22 @@
+//Iterative Way:
+function factorial(num) {
+    if(num === 0) {
+        return -1
+    }
+    let total = 1;
+    for(let i = num; i > 0; i--) {
+        total *= i;
+    }
+    return total;
+}
+
+
+
+
+//Recursive way:
+function factorialRecursive(num) {
+    if(num === 1) {
+        return 1;
+    }
+    return num * factorialRecursive(num -1)
+}
