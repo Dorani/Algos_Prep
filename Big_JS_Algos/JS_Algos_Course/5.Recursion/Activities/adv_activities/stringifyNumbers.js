8d39785111290fc5eff209ebf726c9b9ff866014
Dorani <seif.dorani@gmail.com>
2023-04-01 09:26:54 -0400

    solution is clean, concise and makes perfect sense

--- Big_JS_Algos/JS_Algos_Course/5.Recursion/Activities/adv_activities/stringifyNumbers.js
+++ Big_JS_Algos/JS_Algos_Course/5.Recursion/Activities/adv_activities/stringifyNumbers.js
@@ -58,6 +58,20 @@ let obj = {
 //  * newObj[key] = obj[key]
 //return newObj
 
-function stringifyNumbers(obj) {}
+function stringifyNumbers(obj) {
+  const newObj = {};
+
+  for (let key in obj) {
+    if (typeof obj[key] === "number") {
+      newObj[key] = obj[key].toString();
+    } else if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+
+  return newObj;
+}
 
 stringifyNumbers(obj);
