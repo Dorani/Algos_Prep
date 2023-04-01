c840207c30fbd581f21a15d0ba878b2e551f1f73
Dorani <seif.dorani@gmail.com>
2023-03-31 22:51:15 -0400

    deleted old approach, added in comments for new approach, and detailed out everything, will code and test shortly

--- Big_JS_Algos/JS_Algos_Course/5.Recursion/Activities/adv_activities/stringifyNumbers.js
+++ Big_JS_Algos/JS_Algos_Course/5.Recursion/Activities/adv_activities/stringifyNumbers.js
@@ -1,8 +1,7 @@
 // stringifyNumbers
-// Write a function called stringifyNumbers which takes in an 
-// object and finds all of the values which are numbers and converts 
+// Write a function called stringifyNumbers which takes in an
+// object and finds all of the values which are numbers and converts
 // them to strings. Recursion would be a great way to solve this!
-
 
 /*
 let obj = {
@@ -34,51 +33,31 @@ stringifyNumbers(obj)
 }
 */
 
-
-//Breakdown:
-//Deconstruct obj into an array of values
-    // - use a helper function that takes in input
-    //check to see if there is an input.length === 0
-    //return 0
-    // - check to see if there is an input of type 'number'
-    //if there is, use json.Stringify to convert it into a string
-    //- otherwise if the type of input[0] === 'object'
-    //then deconstruct it and grab the values intialized as newInput
-    // - use helper on newInput
-    // - then before end of helper function
-    // - invoke helper on input, and slice out 1 element at a time until we reach base case
-//invoke helper on initial deconstructured values
-//return object.entries(initial deconstructed array)
-
-
 let obj = {
-    num: 1,
-    test: [],
-    data: {
-        val: 4,
-        info: {
-            isRight: true,
-            random: 66
-        }
-    }
-}
+  num: 1,
+  test: [],
+  data: {
+    val: 4,
+    info: {
+      isRight: true,
+      random: 66,
+    },
+  },
+};
 
-function stringifyNumbers(obj) {
-    let val = Object.values(obj);
+//Recursive approach:
 
-    const helper = (input) => {
-        if(input.length === 0) return '0';
-        if(typeof input[0] === 'number') {
-            JSON.stringify(input[0]);
-        } else if(typeof input[0] === 'object') {
-            let newInput = Object.values(input[0]);
-            helper(newInput);
-        }
-        helper(input.slice(1))
-    }
+//stringifyNumbers(obj):
+//Create an empty newObj = {}
+//loop through each key of the obj function parameter
+// - if the type of the obj[key] === 'number'
+//  * newObj[key]: obj[key].toString();
+// - else if type of obj[key] === 'object' && !Array.isArray(obj[key])
+//  * newObj[key]: stringifyNumbers(obj[key])
+// - else
+//  * newObj[key] = obj[key]
+//return newObj
 
-    helper(val);
-    return Object.assign(val)
-}
+function stringifyNumbers(obj) {}
 
-stringifyNumbers(obj)
+stringifyNumbers(obj);
