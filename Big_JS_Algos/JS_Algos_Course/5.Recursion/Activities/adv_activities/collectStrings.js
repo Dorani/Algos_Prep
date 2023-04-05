17387de00b8c4e02bf70c180ffe0c6fee6bfe2b9
Dorani <seif.dorani@gmail.com>
2023-04-05 16:11:05 -0400

    pure recursion approach

--- Big_JS_Algos/JS_Algos_Course/5.Recursion/Activities/adv_activities/collectStrings.js
+++ Big_JS_Algos/JS_Algos_Course/5.Recursion/Activities/adv_activities/collectStrings.js
@@ -1,31 +1,27 @@
 // CollectStrings
-// Write a function called collectStrings which accepts an object 
-// and returns an array of all the values in the object that have 
+// Write a function called collectStrings which accepts an object
+// and returns an array of all the values in the object that have
 // a typeof string
 
-
-
-
 const obj = {
-    stuff: "foo",
-    data: {
-        val: {
-            thing: {
-                info: "bar",
-                moreInfo: {
-                    evenMoreInfo: {
-                        weMadeIt: "baz"
-                    }
-                }
-            }
-        }
-    }
-}
+  stuff: "foo",
+  data: {
+    val: {
+      thing: {
+        info: "bar",
+        moreInfo: {
+          evenMoreInfo: {
+            weMadeIt: "baz",
+          },
+        },
+      },
+    },
+  },
+};
 
-collectStrings(obj) // ["foo", "bar", "baz"])
+collectStrings(obj); // ["foo", "bar", "baz"])
 
-
-//Breakdown:
+//-------Breakdown Helper Function Approach-------------//
 //Create an empty array to hold the string elements
 //Create helper function to iterate through the object
 // - Check to see if the value of the property is a string
@@ -35,22 +31,42 @@ collectStrings(obj) // ["foo", "bar", "baz"])
 //outside the helper, invoke it, with our obj paramater
 //finally return the new array holding the string elements
 
-
-
 //Helper Function Recursive Style
 function collectStrings(obj) {
-    const stringArr = [];
+  const stringArr = [];
 
-    //helper function to gather strings
-    const gatherStr = (o) => {
-        for(let key in o) {
-            if(typeof o[key] === 'string') {
-                stringArr.push(o[key]);
-            } else if(typeof o[key] === 'object' && !Array.isArray(o[key])) {
-                return gatherStr(o[key]);
-            }
-        }
+  //helper function to gather strings
+  const gatherStr = (o) => {
+    for (let key in o) {
+      if (typeof o[key] === "string") {
+        stringArr.push(o[key]);
+      } else if (typeof o[key] === "object" && !Array.isArray(o[key])) {
+        return gatherStr(o[key]);
+      }
     }
-    gatherStr(obj);
-    return stringArr;
+  };
+  gatherStr(obj);
+  return stringArr;
+}
+
+//-------Breakdown Pure Recursive Approach-------------//
+//Create an empty array to hold the string, arrStr[]
+//Immediately loop through the object
+//Check to see if the obj[key]s value type of is a 'string'
+//If it is, push that obj[key] into the arrStr[]'
+//Check if type of obj[key] is an obj and not an Array
+//return arrStr.concat with the function invoked and pass in the obj[key]
+//return arrayStr;
+
+function collectStrings(obj) {
+  const arrStr = [];
+
+  for (let key in obj) {
+    if (typeof obj[key] === "string") {
+      arrStr.push(obj[key]);
+    } else if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+      return arrStr.concat(collectStrings(obj[key]));
+    }
+  }
+  return arrStr;
 }
