762241ccff4850b8c59aa56a110833034d31da7c
Dorani <seif.dorani@gmail.com>
2023-07-09 23:40:32 -0400

    solution final

--- Big_JS_Algos/Misc_DS_DEV/Leetcode/containsDup2.js
+++ Big_JS_Algos/Misc_DS_DEV/Leetcode/containsDup2.js
@@ -17,7 +17,24 @@
 // Input: nums = [1,2,3,1,2,3], k = 2
 // Output: false
 
+//i and j in the array
+//such that nums[i] == nums[j] and abs(i - j) <= k.
 
+//frequency counter:
 const containsNearbyDuplicate = (nums, k) => {
+  const lookup = {};
 
-}
+  for (let i = 0; i < nums.length; i++) {
+    if (lookup[nums[i]] || lookup[nums[i]] === 0) {
+      if (i - lookup[nums[i]] <= k) {
+        return true;
+      }
+    }
+    lookup[nums[i]] = i;
+  }
+  return false;
+};
+
+const nums = [1, 2, 3, 1, 2, 3],
+  k = 2;
+console.log(containsNearbyDuplicate(nums, k));
