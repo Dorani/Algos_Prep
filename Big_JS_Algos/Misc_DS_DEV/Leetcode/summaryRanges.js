d800493fd190a7d52bf21eecbcaa28ecf21af2a7
Dorani <seif.dorani@gmail.com>
2023-07-18 10:59:46 -0400

    added comments on approach on new problem

---
+++ Big_JS_Algos/Misc_DS_DEV/Leetcode/summaryRanges.js
@@ -0,0 +1,35 @@
+// Example 1:
+
+// Input: nums = [0,1,2,4,5,7]
+// Output: ["0->2","4->5","7"]
+// Explanation: The ranges are:
+// [0,2] --> "0->2"
+// [4,5] --> "4->5"
+// [7,7] --> "7"
+// Example 2:
+
+// Input: nums = [0,2,3,4,6,8,9]
+// Output: ["0","2->4","6","8->9"]
+// Explanation: The ranges are:
+// [0,0] --> "0"
+// [2,4] --> "2->4"
+// [6,6] --> "6"
+// [8,9] --> "8->9"
+
+//create an empty arr called result[]
+//loop through the given array from the begining 0
+//set a start variable = nums[i]
+//while the following condition is met:
+//(first condition makes sure for index is less than entire array length AND)
+//(second condition determines if there is a consecutive order of indexes represented by values)
+    //while (i < entire nums array, &&, nums[i+1] - nums[i] == 1) i++ 
+    //check to see if start != nums[i]
+    //result.push(`${start} -> ${nums[i]}`)
+    //else
+    //result.push(start.toString())
+
+//return result;
+
+const summaryRanges = (nums) => {
+
+}
