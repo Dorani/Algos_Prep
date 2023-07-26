5485f5f9d2997c5ab76a36f9fb026757be52a91c
Dorani <seif.dorani@gmail.com>
2023-07-26 13:05:53 -0400

    added new solution with breakdown, looking to increase speed

--- Big_JS_Algos/Misc_DS_DEV/Leetcode/buy_and_Sell_Stock.js
+++ Big_JS_Algos/Misc_DS_DEV/Leetcode/buy_and_Sell_Stock.js
@@ -1,28 +1,66 @@
 //You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-//You want to maximize your profit by choosing a single day to buy one stock and choosing a 
+//You want to maximize your profit by choosing a single day to buy one stock and choosing a
 //different day in the future to sell that stock.
 
 //Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+// Example 2:
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+
+// Constraints:
+
+// 1 <= prices.length <= 105
+// 0 <= prices[i] <= 104
+
+//check to see if that there are elements, otherwise return 0;
+
+//set two pointers left and right, near each other ie 0,1
+//set your window max that will increase as the pointers move
+//while 1 < the entire arr, since it essentially starts at 1
+//create the profit from the values of the right pointer - left
+//if the profit is greater than the max
+    //make the current max = profit
+
+//if the profit < 0, ie negative number
+//make left = right, so we can slide over and keep going till we get to the end
+
+//right++ regardless of both ifs
+
+//return the max
 
 
-// - Example 1:
-//    Input: nums = [1,2,3,1]
-//    Output: true
-//    Example 2:
+const maxProfit = (prices) => {
+  if (!prices) return 0;
 
-//    Input: nums = [1,2,3,4]
-//    Output: false
-//    Example 3:
+  let left = 0;
+  let right = 1;
+  let max = 0;
 
-//    Input: nums = [1,1,1,3,3,4,3,2,4,2]
-//    Output: true
+  while (right < prices.length) {
+    let profit = prices[right] - prices[left];
+    if (profit > max) {
+      max = profit;
+    }
 
+    if (profit < 0) {
+      left = right;
+    }
+    right++;
+  }
 
-// - Constraints:
+  return max;
+};
 
-//    1 <= nums.length <= 105
-//    -109 <= nums[i] <= 109
+const prices = [7, 1, 5, 3, 6, 4];
 
-
+console.log(maxProfit(prices));
--- Big_JS_Algos/Misc_DS_DEV/Leetcode/buy_and_Sell_Stock.md
+++ Big_JS_Algos/Misc_DS_DEV/Leetcode/buy_and_Sell_Stock.md
@@ -12,21 +12,24 @@
 
  
 
- - Example 1:
+- Example 1:
 
-    Input: nums = [1,2,3,1]
-    Output: true
+    Input: prices = [7,1,5,3,6,4]
+    Output: 5
+    Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+    Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
     Example 2:
 
-    Input: nums = [1,2,3,4]
-    Output: false
-    Example 3:
-
-    Input: nums = [1,1,1,3,3,4,3,2,4,2]
-    Output: true
- 
+    Input: prices = [7,6,4,3,1]
+    Output: 0
+    Explanation: In this case, no transactions are done and the max profit = 0.
+    
 
 - Constraints:
 
-    1 <= nums.length <= 105
-    -109 <= nums[i] <= 109
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+
+
+//2 Pointer Approach:
+
