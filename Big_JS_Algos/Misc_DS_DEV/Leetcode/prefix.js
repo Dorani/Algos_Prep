2a075695e4a3e6dbdaefcf876c8a575992713653
Dorani <seif.dorani@gmail.com>
2023-07-27 19:18:31 -0400

    super optimized, and measures well agaist the comp, left notes

--- Big_JS_Algos/Misc_DS_DEV/Leetcode/prefix.js
+++ Big_JS_Algos/Misc_DS_DEV/Leetcode/prefix.js
@@ -39,3 +39,41 @@ const longestCommonPrefix = (strs) => {
 const strs = ["flower","flow","flight"];
 
 longestCommonPrefix(strs);
+
+
+
+//Refactored version:
+// We create a variable firstStr and assign it the value of the first element in the strs array. 
+// This is because we'll use the characters of the first string as our reference for finding the common prefix.
+
+// We initialize a variable prefix as an empty string. This variable will store the longest common prefix we find.
+
+// We set up a for loop to iterate through the characters of the firstStr 
+    //we create a variable char and assign it the current character at index i of firstStr. 
+    //This character will be our reference character for comparison.
+    //We use the every method to check if the character char at index i is present in the same position in 
+    //all the strings in the strs array. The every method returns true if the provided callback function returns 
+    //true for all elements in the array; otherwise, it returns false.
+
+    // We use the every method to check if the character char at index i is present in the same position in 
+    // all the strings in the strs array. The every method returns true if the provided callback 
+    // function returns true for all elements in the array; otherwise, it returns false.
+
+//After the loop, we return the prefix, which contains the longest common prefix found among all the strings in the strs array.
+const longestCommonPrefixV1 = (strs) => {
+    if(!strs || strs.length === 0) return "";
+
+    const firstWord = strs[0];
+    let prefix = "";
+
+    for(let i = 0; i < firstWord.length; i++) {
+        const char = firstWord[i];
+        if(strs.every(str => str[i] === char)){
+            prefix += char;
+        } else {
+            break;
+        }
+    }
+
+    return prefix;
+}
