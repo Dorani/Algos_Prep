f686ecacc4816ab63dd3d98d8a33a7c5834205d2
Dorani <seif.dorani@gmail.com>
2023-11-24 17:34:59 -0500

    lots of trees

---
+++ Big_JS_Algos/Technical_Interview_Prep/Leetcode/Trees/MaxDepthBinaryTree.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = (root) => {
+  if (!root) return null;
+  let maximum = Math.max(maxDepth(root.left), maxDepth(root.right));
+  return maximum + 1;
+};
