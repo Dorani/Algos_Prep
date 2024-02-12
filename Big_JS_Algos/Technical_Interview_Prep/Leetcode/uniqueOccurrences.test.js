a1b9280c244aad040868b3a1ab0e563ec57dbf3d
Dorani <seif.dorani@gmail.com>
2024-02-12 18:25:54 -0500

    edits to the testing suite

--- Big_JS_Algos/Technical_Interview_Prep/Leetcode/uniqueOccurrences.test.js
+++ Big_JS_Algos/Technical_Interview_Prep/Leetcode/uniqueOccurrences.test.js
@@ -1,27 +1,23 @@
-const uniqueOccurrences = require("./uniqueOccurrences");
+const uniqueOccurrences = require("./Frequency_Counter/uniqueOccurrences");
 
 describe("uniqueOccurrences", () => {
-  test("returns true for array with unique occurrences", () => {
+  test("returns false for array with unique single occurrences, ie if each element's frequecy is equal other", () => {
     const arr = [1, 2, 3, 4];
-    console.log(uniqueOccurrences(arr)); // Logging the result
     expect(uniqueOccurrences(arr)).toBe(false);
   });
 
-  test("returns false for array with non-unique occurrences", () => {
+  test("returns true for array with non-unique occurrences", () => {
     const arr = [1, 2, 2, 3, 3, 3];
-    console.log(uniqueOccurrences(arr)); // Logging the result
     expect(uniqueOccurrences(arr)).toBe(true);
   });
 
   test("returns true for empty array", () => {
     const arr = [];
-    console.log(uniqueOccurrences(arr)); // Logging the result
     expect(uniqueOccurrences(arr)).toBe(true);
   });
 
   test("returns true for single-element array", () => {
     const arr = [42];
-    console.log(uniqueOccurrences(arr)); // Logging the result
     expect(uniqueOccurrences(arr)).toBe(true);
   });
 });
